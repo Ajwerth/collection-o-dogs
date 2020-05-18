8b29a13f24a5e60b7e455faaa85110716cf9b05e
Alec Wertheimer <alec@wintreetz.com>
2020-05-18 15:09:52 -0400

    added styled components, fonts, logo image, and created header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Collection O' Dogs</h1>
-      </header>
+      <Header />
     </div>
   );
 }
