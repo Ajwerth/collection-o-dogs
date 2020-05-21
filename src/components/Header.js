fcce474d1ed8521c2cbf0a3b77884a2c77d04c0e
Alec Wertheimer <alec@wintreetz.com>
2020-05-21 13:02:37 -0400

    clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import logo from '../logo.png';
+import React from "react";
+import logo from "../logo.png";
 import styled from "styled-components";
 
 const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
 
-  .Dog-logo{
+  .Dog-logo {
     width: 80px;
   }
 
-  h1{
+  h1 {
     margin-left: 20px;
   }
 
   margin-bottom: 20px;
-`
+`;
 
 function Header() {
   return (
     <HeaderContainer>
-        <img src={logo} className="Dog-logo" alt="logo" />
-        <h1> Collection O' Dogs</h1>
+      <img src={logo} className="Dog-logo" alt="logo" />
+      <h1> Collection O' Dogs</h1>
     </HeaderContainer>
   );
 }
